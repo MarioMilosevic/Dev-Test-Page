4f1b145966c7a31e20feddd34e851e3d1eb9005d
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-04 14:14:27 +0200

    Add responsive header navigation

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 import './sass/main.scss'
+'use strict'
+
+const navigation = document.querySelector(".header__list-hamburger");
+const mobileNavigation = document.querySelector(".header__mobileList");
+const xButton = document.querySelector(".header__mobileList-button");
+
+
+const openNavigation = () => {
+    mobileNavigation.style.transform = "translateX(0%)";
+    navigation.style.display = 'none'
+}
+
+const closeNavigation = () => {
+     mobileNavigation.style.transform = "translateX(100%)";
+     navigation.style.display = "block";
+}
+navigation.addEventListener('click', openNavigation)
+xButton.addEventListener('click', closeNavigation)
 
