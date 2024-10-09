f613274433137c15eccaf61699ebbc4315fd4b0d
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-09 13:51:11 +0200

    Refactor code

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-import './sass/main.scss'
-'use strict'
+import "./sass/main.scss";
+("use strict");
 
 const navigation = document.querySelector(".header__list-hamburger");
 const mobileNavigation = document.querySelector(".header__mobileList");
 const xButton = document.querySelector(".header__mobileList-button");
 
-
 const openNavigation = () => {
-    mobileNavigation.style.transform = "translateX(0%)";
-    navigation.style.display = 'none'
-}
+  mobileNavigation.style.visibility = "visible";
+  mobileNavigation.style.transform = "translateX(0%)";
+  navigation.style.display = "none";
+};
 
 const closeNavigation = () => {
-     mobileNavigation.style.transform = "translateX(100%)";
-     navigation.style.display = "block";
-}
-navigation.addEventListener('click', openNavigation)
-xButton.addEventListener('click', closeNavigation)
-
+  mobileNavigation.style.visibility = "hidden";
+  mobileNavigation.style.transform = "translateX(100%)";
+  navigation.style.display = "block";
+};
+navigation.addEventListener("click", openNavigation);
+xButton.addEventListener("click", closeNavigation);
